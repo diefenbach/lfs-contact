a257694efd2566d7fd2e74bcf996a927d84c4fd1
Kai Diefenbach <kai.diefenbach@iqpp.de>
2016-02-23 13:37:12 +0100

    Adds attachments.

--- lfs_contact/utils.py
+++ lfs_contact/utils.py
@@ -2,6 +2,7 @@
 import datetime
 
 # django imports
+from django import forms
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.template import RequestContext
@@ -23,10 +24,11 @@ def send_contact_mail(request, form, template="lfs/mail/contact_mail.html"):
 
     fields = []
     for field_name, field in form.fields.items():
-        fields.append({
-            "label": _(field.label.title()),
-            "value": form.cleaned_data.get(field_name)
-        })
+        if not isinstance(field, forms.FileField):
+            fields.append({
+                "label": _(field.label.title()),
+                "value": form.cleaned_data.get(field_name)
+            })
 
     text = render_to_string(template, RequestContext(request, {
         "date": datetime.datetime.now(),
@@ -47,7 +49,16 @@ def send_contact_mail(request, form, template="lfs/mail/contact_mail.html"):
         'Sender': shop.from_email
     }
 
-    mail = EmailMultiAlternatives(subject=subject, body="", from_email=from_email, to=to, bcc=bcc, headers=headers)
+    mail = EmailMultiAlternatives(subject=subject, body=text, from_email=from_email, to=to, bcc=bcc, headers=headers)
     mail.attach_alternative(text, "text/html")
+
+    # Attach files
+    for field_name, field in form.fields.items():
+        if isinstance(field, forms.FileField):
+            try:
+                data = form.cleaned_data.get(field_name)
+                mail.attach(data.name, data.read())
+            except:
+                pass
 
     mail.send()
