76bea646510f8076fc890a9e7f3c7a6099825795
Kai Diefenbach <kai.diefenbach@iqpp.de>
2017-02-02 08:42:36 +0100

    Adds slashes at the end of URLs to make APPEND_SLASH
    This makes APPEND_SLASH work properly.

--- lfs_contact/urls.py
+++ lfs_contact/urls.py
@@ -2,6 +2,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('lfs_contact.views',
-    url(r'^contact$', "contact_form", name='lfs_contact_form'),
+    url(r'^contact/$', "contact_form", name='lfs_contact_form'),
     url(r'^contact-form-sent/$', "contact_form_sent", name='lfs_contact_form_sent'),
 )
