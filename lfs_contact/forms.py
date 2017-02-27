9b3705353eebef849eea7ba14a2d555ab0a7abc8
Maciej Wiśniowski <pigletto@gmail.com>
2017-02-27 19:22:33 +0100

    added polish and french translations; added default title for questions about product availability

--- lfs_contact/forms.py
+++ lfs_contact/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import HiddenInput
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -7,4 +8,5 @@ class ContactForm(forms.Form):
     """
     name = forms.CharField(label=_(u'Name'))
     email = forms.EmailField(label=_(u'E-Mail'))
+    subject = forms.CharField(label=_(u'Subject'), widget=HiddenInput, required=False)
     message = forms.CharField(label=_(u'Message'), widget=forms.Textarea())
