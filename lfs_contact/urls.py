a83e886a406237794ee0d66d20968cb2a881038b
Kai Diefenbach <kai.diefenbach@iqpp.de>
2025-02-28 14:09:44 +0100

    Refactor URL patterns to use path() instead of re_path() for contact views

--- lfs_contact/urls.py
+++ lfs_contact/urls.py
@@ -1,7 +1,8 @@
-from django.urls import re_path
+from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    re_path(r"^contact/$", views.contact_form, name="lfs_contact_form"),
-    re_path(r"^contact-form-sent/$", views.contact_form_sent, name="lfs_contact_form_sent"),
+    path("contact", views.contact_form, name="lfs_contact_form"),
+    path("contact-form-sent", views.contact_form_sent, name="lfs_contact_form_sent"),
 ]
